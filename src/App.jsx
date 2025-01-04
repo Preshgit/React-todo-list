f7b0d650183591caf56637d10e695a28a7011fd1
preshgit <preciousdesk10@gmail.com>
2025-01-04 19:33:44 +0100

    creat todo-list using react

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  useState("");
+  return (
+    <>
+      <form className="new-item-form" action="">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 1
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            Item 2
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+      </ul>
+    </>
+  );
+}
